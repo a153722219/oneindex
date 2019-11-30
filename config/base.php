a874498928b2e5de772ef04fe342f490bd3ef900
a153722219 <437816510@qq.com>
2019-11-30 18:51:24 +0800

    #

--- config/base.php
+++ config/base.php
@@ -1,8 +1,8 @@
 <?php return array (
-  'site_name' => 'OneIndex',
-  'password' => 'oneindex',
+  'site_name' => 'baiindex',
+  'password' => 'Aa36987412',
   'style' => 'material',
-  'onedrive_root' => '',
+  'onedrive_root' => '/',
   'cache_type' => 'secache',
   'cache_expire_time' => 3600,
   'cache_refresh_time' => 600,
@@ -101,4 +101,6 @@
   'client_id' => 'a49f9a25-836a-4da1-b18e-f23263a681b5',
   'redirect_uri' => 'https://oneindex.github.io/',
   'refresh_token' => 'OAQABAAAAAACQN9QBRU3jT6bcBQLZNUj7K1T7ZN4NYivBKZRhVwE0ahlv2_C36xgbWZlVtkRpgogErykq6wtinxQrXMIhDWDhXJNz7HALc8ODDjFAAuqvvH4Y0XPI3V536VicK_1oscD2pQYegWQGtyfn6vOvHdCngeHY9dLSWPzqfpkrxg4sQPksnfXnjNA_SA9xQl8pu6g202c0dLQlS8fjeI7z_SqUIiTEnZZru6t5p6eQniKGFr8wtPJVzbo6jyUMjQNk4omO9p4-NQzbN9UgJu2ChPdtWxFsbver2rfEPa5wVgzQGnp1vBlrRjc3JlgQ02ouD3tJWv_e0glqRG4NGXkKSH373LnymTVqA-y1o_1I2BEnJHHp89xt9RnI0et02qtt6woCs6g4rP4qr-xLVjaiUTAhAz3V9hy_pXHHylHveROTwnig5d2Iw0MNkKG58tHn_BqpCtEO5HxN4Hc5Znk0D_cno5dbeveepdwLLus5Y7pbM_H5IsLJ2MN9ztS5E1E268LJ3_q20x5BpZZ6qjIfWkl2FByRBVbUAtYjK1RkpNXDpR5_CaajToGq0gRUbD4bIL6TUmvzjN9O88LxycDEVm8wT73L_nO3clRAq9hu9J9Bzbo8vPTxdUBdkHo964Wpej6ir9CJ-KvjaeN0m9ayrHFclD6ZIVCayry4gr3BtnmjwDzMjb1Z4u3ehNcHif4GKIxdYgVSuQ6o748JIL57fpQEeqevcYCicsiCDjjGl35a_tXETG8JWozeRCIdS0JnM-cQ0Qtzas4o1decwwLuURdtf1UqEu8Bx8ysQIdhaXYV9NnXSpMay5OFE9qZ6ksSk0jFZ90Kva7dRx08eMmCN-oGIAA',
+  'onedrive_hide' => '/uploads',
+  'onedrive_hotlink' => '',
 );
